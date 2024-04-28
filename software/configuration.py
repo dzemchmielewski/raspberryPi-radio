e552005d44988534bff9efe56437930c837ae328
Pawel Chmielewski <chmielu@ais.pl>
2024-04-28 23:22:49 +0200

    Stream added

--- software/configuration.py
+++ software/configuration.py
@@ -17,6 +17,7 @@ STATIONS = (
     Station("Radio Nowy Swiat", "RNS", "http://stream.rcs.revma.com/ypqt40u0x1zuv"),
     Station("TOK FM", "TOKFM", "http://poznan5-4.radio.pionier.net.pl:8000/tuba10-1.mp3"),
     Station("Los 40 Classic", "LOS40CS", "https://20133.live.streamtheworld.com/LOS40_CLASSICAAC_SC"),
+    Station("RFI Monde", "RFIMON", "https://rfimonde64k.ice.infomaniak.ch/rfimonde-64.mp3"),
     Station("Radio 357", "R357", "https://stream.radio357.pl"),
     Station("Trójka", "PR3", "http://mp3.polskieradio.pl:8904/"),
     Station("Antyradio Classic Rock", "ANTCLA", "https://an04.cdn.eurozet.pl/ANTCLA.mp3"),
